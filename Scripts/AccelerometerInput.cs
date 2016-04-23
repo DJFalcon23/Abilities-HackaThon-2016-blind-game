c2fcff32200b5435d3e2b2bbcd806cee5e328209
bellapalumbi <bpalumbi@youth.dhftc.org>
2016-04-23 14:55:58 -0400

    2:55 pm Saturday

---
+++ Scripts/AccelerometerInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+
+
+	void Update () 
+	{
+		transform.Translate ((Input.acceleration.x)/2, 0, (-Input.acceleration.z)/2);
+	}
+}
