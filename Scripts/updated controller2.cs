c476c60a50b67e8122f1e0d00bda7ca47dea8a1b
Derek J. Fields Jr <Whitefalcon003@aol.com>
2016-04-23 18:27:38 -0400

    Create updated controller2.cs

---
+++ Scripts/updated controller2.cs
@@ -0,0 +1,23 @@
+var speed = 10.0;
+function Update () {
+    var dir : Vector3 = Vector3.zero;
+
+    // we assume that the device is held parallel to the ground
+    // and the Home button is in the right hand
+
+    // remap the device acceleration axis to game coordinates:
+    // 1) XY plane of the device is mapped onto XZ plane
+    // 2) rotated 90 degrees around Y axis
+    dir.x = -Input.acceleration.y;
+    dir.z = Input.acceleration.x;
+
+    // clamp acceleration vector to the unit sphere
+    if (dir.sqrMagnitude > 1)
+        dir.Normalize();
+
+    // Make it move 10 meters per second instead of 10 meters per frame...
+    dir *= Time.deltaTime;
+
+    // Move object
+    transform.Translate (dir * speed);
+}
