ea3d23961ff51d61903b3cac386103f965d53e37
Derek J. Fields Jr <Whitefalcon003@aol.com>
2016-04-23 19:32:03 -0400

    Added files via upload

---
+++ Scripts/update controller3.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	public float speed = 10.0F;
+	void Update() {
+		Vector3 dir = Vector3.zero;
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+
+		dir *= Time.deltaTime;
+		transform.Translate(dir * speed);
+	}
+}
